4d6f47dab63db9f9144791e5a5693494e87cab59
tubone24 <tubo.yyyuuu@gmail.com>
2019-12-01 22:09:06 +0900

    layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,16 @@
 <template>
-  <section class="section">
-    <div class="container">
-      <hello  propHello="HelloComposition"/>
-    </div>
-  </section>
+  <div class="home">
+    <h1>Ebook Homebrew Home</h1>
+    <br><br>
+    <img alt="ebook logo" src="../assets/image/logo.png">
+    <br><br>
+    <h2>How to use it?</h2>
+    <a href="https://ebook-homebrew.readthedocs.io/en/latest/">ebook-homebrew Docs</a><br>
+    <a href="https://ebook-homebrew.herokuapp.com/docs">ebook-homebrew API Ref</a><br>
+    Or try to click "Sample" at Top Nav bar, Use <b>ebook-homebrew Nuxt.js</b> client.
+  </div>
 </template>
 
-<script lang="ts">
-  import {
-    createComponent,
-    reactive,
-    onMounted,
-    computed,
-    ref
-  } from '@vue/composition-api';
-  import Hello from '@/components/Hello.vue';
+<style>
 
-  export default createComponent({
-    layout: 'default',
-    components: {
-      Hello
-    },
-    setup() {
-    }
-  })
-
-</script>
+</style>
