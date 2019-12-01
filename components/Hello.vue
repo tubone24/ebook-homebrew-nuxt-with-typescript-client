67f8904eafcb7d1165e995bcc624dbe8917af32d
tubone24 <tubo.yyyuuu@gmail.com>
2019-12-01 16:37:04 +0900

    wip

--- components/Hello.vue
+++ components/Hello.vue
@@ -1,46 +1,27 @@
 <template>
-  <div class="FileUpload" id="FileUpload">
-    <div>
-      <select id="format-select" v-model="selected">
-        <option disabled value="">Please select one</option>
-        <option value="image/png">image/png</option>
-        <option value="image/jpeg">image/jpeg</option>
-        <option value="image/gif">image/gif</option>
-      </select>
-      <span id="selected-format">Image Format: {{ selected }}</span>
-    </div>
-    <div v-if="!image" id="select-not-yet-image">
-      <h2>Select images</h2>
-      <input id="file-choice" type="file" @change="onFileChange" multiple="multiple" accept="image/*">
-    </div>
-    <div v-else id="selected-images">
-      <div v-if="isLoading">
-        <div v-show="isLoading" id="post-file-loader" class="loader">Post File...</div>
-      </div>
-      <div v-else>
-        <img :src="image" alt="select image"/>
-      </div>
-      <button id="remove-image" class="btn btn-danger" @click="removeImage">Remove images</button>
-      <div v-if="selected && image">
-        <button id="post-image" class="btn btn-primary" @click="postImage">Post images</button>
-      </div>
-      <div v-if="this.$store.getters['fileUpload/getUploadId']">
-        <span id="upload-id">UploadId: {{ this.$store.getters['fileUpload/getUploadId'] }}</span>
-      </div>
-      <div v-if="this.$store.getters['fileUpload/getUploadId']">
-        <div class="row">
-          <div class="col-sm-6">
-            <button id="convert-images" class="btn btn-info" @click="convertImages">Convert images</button>
-          </div>
-          <div v-if="converted" class="col-sm-6">
-            <button id="download-pdf-200" class="btn btn-success" @click="downloadPDF">Download PDF</button>
-          </div>
-          <div v-else class="col-sm-6">
-            <button id="download-pdf-404" class="btn btn-warning" @click="downloadPDF">Download PDF</button>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div id="list">
+    <table>
+      <thead>
+      <tr>
+        <th class="id">ID</th>
+        <th class="filepath">File Path</th>
+        <th class="lastindex">Last Index</th>
+        <th class="type">File Type</th>
+        <th class="updated">Updated at</th>
+        <th class="download">-</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="item in uploadList">
+        <th>{{ item.id }}</th>
+        <th>{{ item.file_path }}</th>
+        <th>{{ item.last_index }}</th>
+        <th>{{ item.file_type }}</th>
+        <th>{{ item.updated_at }}</th>
+        <button v-on:click="doDownload(item.file_path)">DL</button>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -55,54 +36,14 @@
   import axios from "axios";
   import { AxiosPromise } from "axios";
 
-  const getDataFromBitcoin = () => {
-    let info: any = ref(); // OK
-    // let info: any = {}; // NG
-    // refがないとreactiveにならない為、dataの中身が変わっても検知されない
+  const backendURL = 'https://ebook-homebrew.herokuapp.com/';
 
-    // methods
-    const getData = () => {
-      axios
-        .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-        .then(response => (info.value = response));
-      // refで定義された変数にはvalue.propertyでアクセスが可能
-      // refのvalueにobjectが定義された場合、深い階層までリアクティブになる
-    };
+  const state = reactive<{
+    uploadList: Array<Map<string, string>>
+  }>({
+    uploadList: []
+  });
 
-    // mounted
-    onMounted(() => {
-      getData();
-    });
-
-    // 使用するデータは全てreturn
-    return {
-      info,
-      getData
-    };
-  };
-
-  /**
-   * ボタンをクリックすると数値がインクリメントし、かつ2倍にした数を表示
-   */
-  const countDouble = () => {
-    // data
-    const state: any = reactive({
-      count: 0,
-      // computed
-      double: computed(() => state.count * 2)
-    });
-
-    // methods
-    const increment = () => {
-      state.count++;
-    };
-
-    // 使用するデータは全てreturn
-    return {
-      state,
-      increment
-    };
-  };
 
   type Props = {
     propHello: string;
@@ -114,31 +55,16 @@
         type: String
       }
     },
-    setup(props: Props) {
+    setup (props: Props) {
       // props
       const propsHello = props.propHello;
 
       // methods
-      const BitCoinData = getDataFromBitcoin();
-      const CountDouble = countDouble();
 
       // data
-      const state = reactive<{
-        image: string;
-        selected: string;
-        converted: boolean;
-        isLoading: boolean;
-      }>({
-        image: '',
-        selected: '',
-        converted: false,
-        isLoading: false,
-      });
 
       // 使用するデータは全てreturn
       return {
-        BitCoinData,
-        CountDouble,
         state,
         propsHello
       };
