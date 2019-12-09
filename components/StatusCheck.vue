f3ccb76cfe4c66a752b2dc250928b2467ade1fe6
tubone24 <tubo.yyyuuu@gmail.com>
2019-12-09 15:23:07 +0900

    fix reactive

--- components/StatusCheck.vue
+++ components/StatusCheck.vue
@@ -85,17 +85,17 @@
   }
 
   #get-status {
+    display: block;
     width: 280px;
+    margin: 10px auto;
+    padding: 10px 0;
     border-radius: 3px;
     box-shadow: 0 3px 0 rgba(136,136,136,1);
-    display: block;
     font-size: 18px;
     font-weight: bold;
     color: #fff;
     text-align: center;
     text-decoration: none;
-    margin: 10px auto;
-    padding: 10px 0;
   }
   #get-status :hover {
     background-color:#79acdf;
@@ -104,12 +104,12 @@
     box-shadow:inset 0px 0px 3px #fff;
   }
   #get-status :active{
-    padding-top:6px;
-    padding-bottom:4px;
-    border:1px solid #334c66;
-    background-color:#69c;
-    color:#e0ebf5;
-    -webkit-box-shadow:inset 0px 0px 8px #334c66;
+    padding-top: 6px;
+    padding-bottom: 4px;
+    border: 1px solid #334c66;
+    background-color: #69c;
+    color: #e0ebf5;
+    -webkit-box-shadow: inset 0px 0px 8px #334c66;
     -moz-box-shadow:inset 0px 0px 2px #3a6da0;
     box-shadow:inset 0px 0px 2px #3a6da0;
   }
